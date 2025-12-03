a73c6aaa8e08fdcfd1d623a9ea9b72802eb745ab
Nanz <113892126+iruthaya-nancy@users.noreply.github.com>
2025-12-03 20:50:53 +0530

    Time: 1 ms (64.58%) | Memory: 46.8 MB (7.59%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,6 +3,8 @@ class Solution {
         /**
         we can't use the set because this code requires the in-place logic so no
         extra spaces and the variable.So we have to change in the input array
+        In this we are iterating from 1 Because THE FIRST ELEMENT OF THE ARRAY WILL BE 
+        ALWAYS SORTED.
          */
         int k = 1;
         for(int i = 1 ; i< nums.length ; i++){
