2344d190a5731a89e7ea26fa4f404c9cd5909f74
Nanz <113892126+iruthaya-nancy@users.noreply.github.com>
2025-12-03 20:49:36 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (43.72%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        /**
+        we can't use the set because this code requires the in-place logic so no
+        extra spaces and the variable.So we have to change in the input array
+         */
+        int k = 1;
+        for(int i = 1 ; i< nums.length ; i++){
+            if(nums[i-1] != nums[i]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
