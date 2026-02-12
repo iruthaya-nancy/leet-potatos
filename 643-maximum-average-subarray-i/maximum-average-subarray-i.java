4f36e7273f13fdcd781e7c12643b8ebe6fd4d0d7
Nanz <113892126+iruthaya-nancy@users.noreply.github.com>
2026-02-12 23:49:56 +0530

    Time: 2 ms (99.80%) | Memory: 69.7 MB (41.08%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        /**
+        To find the max average with the window size of k
+        1.  Sum the values till the k length
+        2. Then for the iteration intermediate values will be same and only one value will be added
+        and one will be removed */
+        int sum = 0;
+        for(int i = 0; i<k;i++ ){
+            sum += nums[i];
+        }
+
+        int maxVal = sum;
+        for(int i = k ; i< nums.length;i++){
+            sum = sum - nums[i-k]+nums[i];
+            maxVal = Math.max(sum,maxVal);
+        }
+
+        return (double)maxVal/k;
+    }
+}
