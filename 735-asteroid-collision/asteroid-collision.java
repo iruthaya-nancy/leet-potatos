b324135cb23642a14668ad8373ebe59903b56d6a
Nanz <113892126+iruthaya-nancy@users.noreply.github.com>
2026-02-19 01:35:08 +0530

    Time: 4 ms (81.46%) | Memory: 47 MB (71.45%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,32 +1,34 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-         //use while loop for iteration->we dont know exact iteration
-         List<Integer> asList = Arrays.stream(asteroids)
-        .mapToObj(Integer::valueOf).collect(Collectors.toList());
-          int i = 0;
-        while (i < asList.size() - 1) {
-            int current = asList.get(i);
-            int next = asList.get(i + 1);
-
-            if (current > 0 && next < 0) {
-                if (Math.abs(current) == Math.abs(next)) {
-                    asList.remove(i);
-                    asList.remove(i);
-                    if (i > 0) {
-                        i--;
-                    }
-                } else if (Math.abs(current) > Math.abs(next)) {
-                    asList.remove(i + 1);
-                } else {
-                    asList.remove(i);
-                    if (i > 0) {
-                        i--;
-                    }
+        Stack<Integer> val = new Stack<>();
+        for(int a : asteroids){
+            boolean alive = true;
+            while(alive && a < 0 && !val.isEmpty() && val.peek() > 0){
+                int top = val.peek();
+                if(top < -a){
+                    //collision remve the smaller value
+                    val.pop();
                 }
-            } else {
-                i++;
+                else if(top == -a){
+                    //entire collision
+                    alive = false;
+                    val.pop();
+                }
+                else{
+                    alive = false; // |a| modular case
+                }
             }
+
+                if(alive){
+                    val.push(a);
+                }
         }
-         return asList.stream().mapToInt(Integer::intValue).toArray();
+                
+        // Convert stack to array
+        int[] result = new int[val.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = val.pop();
+        }
+        return result;
     }
 }
